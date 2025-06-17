af2b02d4bc8133b520c1164027664265e2de1ecc
Tan Wee Joe <84664178+w3joe@users.noreply.github.com>
2025-06-17 09:16:17 +0800

    feature: add update progress tracker

--- frontend/services/progressTrackerService.ts
+++ frontend/services/progressTrackerService.ts
@@ -1,15 +1,30 @@
 import { ProgressTracker } from "@/models/ProgressTracker";
 import api from "../api/api";
 
-export const createProgressTracker = async (progressTrackerData: ProgressTracker) => {
+export const createProgressTracker = async (
+  progressTrackerData: ProgressTracker
+) => {
   try {
-    const response = await api.post(
-      "/progress/status",
+    const response = await api.post("/progress/status", progressTrackerData);
+    return response.data;
+  } catch (error) {
+    console.error("Failed to create lesson:", error);
+    throw error;
+  }
+};
+
+export const updateProgressTracker = async (
+  progress_id: string,
+  progressTrackerData: ProgressTracker
+) => {
+  try {
+    const response = await api.put(
+      `/progress/status/${progress_id}`,
       progressTrackerData
     );
     return response.data;
   } catch (error) {
-    console.error("Failed to create lesson:", error);
+    console.error("Failed to update progress tracker:", error);
     throw error;
   }
 };
